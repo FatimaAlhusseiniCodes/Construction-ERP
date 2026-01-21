4a2b22d0b26f10677e9cbe16a8584b0b1c6aef36
hudashalhoub662 <hudashalhoub662@gmail.com>
2026-01-21 23:23:26 +0200

    financial part

--- app/Filament/App/Resources/ClientResource/Pages/CreateClient.php
+++ app/Filament/App/Resources/ClientResource/Pages/CreateClient.php
@@ -1,11 +1,25 @@
 <?php
-
 namespace App\Filament\App\Resources\ClientResource\Pages;
-
+use Illuminate\Validation\ValidationException;
 use App\Filament\App\Resources\ClientResource;
 use Filament\Resources\Pages\CreateRecord;
+use Filament\Facades\Filament;
 
 class CreateClient extends CreateRecord
 {
     protected static string $resource = ClientResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+ {
+        $companyId=auth()->user()?->company_id;
+        if(! $companyId){
+            throw ValidationException::withMessages([
+                'company_id' => 'your user is not linked to a company. Set company_id for your user first.',
+            ]);
+        }
+
+        $data['company_id'] = $companyId;
+
+        return $data;
+ }
 }
